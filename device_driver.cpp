314a60f08e3bb21c129ba4d9d944f3905b8ca4d8
kdh <dongri.kang@sansumg.com>
2025-04-15 09:43:57 +0900

    read success

--- device_driver.cpp
+++ device_driver.cpp
@@ -1,4 +1,14 @@
 #include "device_driver.h"
+#include <stdexcept>
+
+#define REPEAT (4)
+
+class ReadFailException : public std::exception
+{ 
+public:
+    using _Mybase = std::exception;
+    explicit ReadFailException(const char* _Message) : _Mybase(_Message) {}
+};
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice *hardware) : m_hardware(hardware)
 {}
@@ -6,7 +16,15 @@ DeviceDriver::DeviceDriver(FlashMemoryDevice *hardware) : m_hardware(hardware)
 int DeviceDriver::read(long address)
 {
     // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    unsigned char ret = m_hardware->read(address);
+    for (int i=0; i<REPEAT; i++)
+    {
+        if (ret != m_hardware->read(address))
+        {
+            throw ReadFailException("Reliability read failed");
+        }
+    }
+    return (int)(ret);
 }
 
 void DeviceDriver::write(long address, int data)
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "gmock/gmock.h"
 #include "device_driver.h"
+#include "mock_flash.h"
 
 int main()
 {
@@ -7,11 +8,23 @@ int main()
     return RUN_ALL_TESTS();
 }
 
-TEST(DeviceDriver, ReadFromHW)
+// TEST(DeviceDriver, ReadFromHW)
+// {
+//     // TODO : replace hardware with a Test Double
+//     FlashMemoryDevice *hardware = nullptr;
+//     DeviceDriver driver{ hardware };
+//     int data = driver.read(0xFF);
+//     EXPECT_EQ(0, data);
+// }
+
+TEST(DeviceDriver, ReadFromHWSuccess)
 {
-    // TODO : replace hardware with a Test Double
-    FlashMemoryDevice *hardware = nullptr;
-    DeviceDriver driver{ hardware };
+    MockFlash stMockFlash;
+    EXPECT_CALL(stMockFlash, read(0xFF))
+        .Times(5)
+        .WillRepeatedly(testing::Return(0));
+
+    DeviceDriver driver{ &stMockFlash };
     int data = driver.read(0xFF);
     EXPECT_EQ(0, data);
 }
